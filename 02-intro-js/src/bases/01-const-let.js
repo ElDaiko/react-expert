18c3d0dd315e34f6c835902e16aee2a51261d38a
Miguel Roldán Echeverri <miguel.roldan@pragma.com.co>
2024-03-31 18:52:41 -0500

    feat: initial commit

---
+++ 02-intro-js/src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+/// Variables y constantes
+const nombre = 'Miguel'
+const apellido = 'Roldán'
+
+let valorDado = 5
+valorDado = 4
+
+console.log({nombre, apellido, valorDado});
+if (true) {
+    let valorDado = 6
+
+    console.log(valorDado);
+}
