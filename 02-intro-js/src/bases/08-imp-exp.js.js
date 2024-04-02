a6ea394e98764367daf40c0335d1d69becbeb45c
Miguel Roldán <mirole16@gmail.com>
2024-04-02 16:01:01 -0500

    feat: 08

---
+++ 02-intro-js/src/bases/08-imp-exp.js.js
@@ -0,0 +1,16 @@
+/* import {heroes} from "./data/heroes"; */
+import {heroes, owners}  from "../data/heroes"; //imp
+
+console.log(owners);
+
+
+//find
+const getHeroeById = (id) => {
+    return heroes.find((heroes) => heroes.id === id)
+}
+console.log(getHeroeById(2));
+
+//filter
+const getHeroesByOwner = (owner) => heroes.filter((heroes) => heroes.owner === owner)
+
+console.log(getHeroesByOwner('DC'));
