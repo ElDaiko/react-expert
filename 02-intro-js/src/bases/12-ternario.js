ddcaac1aac64dac4d877fae05d6e558d475b3322
Miguel Roldán <mirole16@gmail.com>
2024-04-04 10:32:40 -0500

    feat: 12

---
+++ 02-intro-js/src/bases/12-ternario.js
@@ -0,0 +1,14 @@
+const activo = true
+
+/* let mensaje = '';
+
+if (activo) {
+    console.log('activo');
+}else{
+    console.log('Inactivo');
+} */
+
+/* const mensaje = (activo) ? 'Activo' : 'inactivo' */
+const mensaje = activo && 'Activo'
+
+console.log(mensaje);
